a4088f69b45fa875a4708d38b00a31b0d5c89710
María Cano <maria.cano4435@alumnos.udg.mx>
2020-10-15 21:10:31 -0500

    prueba A Slices

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var si []uint64
+	var c, i, d uint64
+	fmt.Scan(&c)
+	d = c
+	for i = 0; i < d; {
+		fmt.Scan(&c)
+		si = append(si, c)
+		i = i + 1
+	}
+	//fmt.Println(si)
+	c = 0
+	for i = 0; i < d; {
+		c = c + si[i]
+		//fmt.Println(c)
+		i = i + 1
+	}
+	fmt.Println(c)
+
 }
